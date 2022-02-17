0dba2b15589e460f62bdc43b64c29bcf57bf370c
IG <iikim2511@gmail.com>
2022-02-17 13:34:07 -0500

    skills-fin

--- main.js
+++ main.js
@@ -20,23 +20,24 @@ div.innerHTML=`
 `;
 aboutMajors.appendChild(div);
  */
-class Majors {
-  constructor(a_icon,b_title){
-      let div = document.createElement('div');
-      div.innerHTML=`
-          <div class="major">
-          <div class="major__icon">${a_icon}</div>
-          <h2 class="major__title">${b_title}</h2>
-          </div>
-      `;
-      aboutMajors.appendChild(div);
-  }
-}
 
-let newMajor = new Majors(`<i class="fab fa-html5"></i>`,"html");
-let newMajor2 = new Majors(`<i class="fab fa-css3-alt"></i>`,"CSS");
-let newMajor3 = new Majors(`<i class="fab fa-js-square"></i>`,"Javascript");
-let newMajor4 = new Majors(`<i class="fab fa-git"></i>`,"git");
+// class Majors {
+//   constructor(a_icon,b_title){
+//       let div = document.createElement('div');
+//       div.innerHTML=`
+//           <div class="major">
+//           <div class="major__icon">${a_icon}</div>
+//           <h2 class="major__title">${b_title}</h2>
+//           </div>
+//       `;
+//       aboutMajors.appendChild(div);
+//   }
+// }
+
+// let newMajor = new Majors(`<i class="fab fa-html5"></i>`,"html");
+// let newMajor2 = new Majors(`<i class="fab fa-css3-alt"></i>`,"CSS");
+// let newMajor3 = new Majors(`<i class="fab fa-js-square"></i>`,"Javascript");
+// let newMajor4 = new Majors(`<i class="fab fa-git"></i>`,"git");
 
 
 
